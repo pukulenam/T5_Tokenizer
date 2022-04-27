e3b4f779f89959f15d0c78e2920d8075221b20b5
gabrielkheisa <gabrielkheisa@gmail.com>
2022-04-27 19:02:50 +0700

    add

--- frontend/change.py
+++ frontend/change.py
@@ -4,7 +4,7 @@ url = 'http://catfeeder.gabrielkheisa.xyz/posting.php'
 
 params = dict(
     api_key='8c51ece6',
-    device_status='0'
+    device_status='0',
 )
 
 resp = requests.get(url=url, params=params)

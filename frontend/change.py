926e5ff48b77a077bc3d62d0abe1d2f07fbbf637
gabrielkheisa <gabrielkheisa@gmail.com>
2022-04-28 13:27:42 +0700

    a

---
+++ frontend/change.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'http://catfeeder.gabrielkheisa.xyz/posting.php'
+
+params = dict(
+    api_key='8c51ece6',
+    device_status='0',
+)
+
+resp = requests.get(url=url, params=params)
+
+# hasil di http://catfeeder.gabrielkheisa.xyz/viewing.php

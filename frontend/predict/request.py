44af884f5023c49e12935314b08b461a042800d8
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2022-04-28 11:31:10 +0700

    adding python request to api

---
+++ frontend/predict/request.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+import json
+with open('parse.json') as f:
+    data = json.load(f)
+
+API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
+headers = {"Authorization": f"Bearer {'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'}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": data['input-text'],
+})
+
+print(output)
+
+a_dict = {'summarize': 'Intinya... ' + output[0]['summary_text']}
+
+data.update(a_dict)
+
+with open('parse.json', 'w') as f:
+    json.dump(data, f)

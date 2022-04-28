f02fb3bebe7a5f0b1e57865ff8e6b4432342abbb
AzisPrasetyo <azissofyanp@gmail.com>
2022-04-28 12:18:00 +0700

    add proper form and get range val

---
+++ frontend/header.php
@@ -0,0 +1,27 @@
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>T5</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="assets/vendor/google-fonts/google-fonts.css" rel="stylesheet">
+  
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
--- frontend/index.php
+++ frontend/index.php
@@ -1,43 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>T5</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="assets/vendor/google-fonts/google-fonts.css" rel="stylesheet">
-  
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-</head>
+<?php
+include 'header.php';
+?>
 
 <body>
 
   <main>
     <div class="container">
 
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
+      <section class="section d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container g-2">
           <div class="row justify-content-center">
-            <div class="col-lg-4 d-flex flex-column align-items-center justify-content-center">
+            <div class="col-lg-8 d-flex flex-column align-items-center justify-content-center">
 
               <div class="d-flex justify-content-center py-4">
                 <a href="#" class="logo d-flex align-items-center w-auto">
@@ -46,26 +22,121 @@
                 </a>
               </div><!-- End Logo -->
 
-              <div class="card mb-3">
-
+              <div class="card col-lg-12">
                 <div class="card-body">
-                  <div class="pt-2">
-                    <h5 class="card-title text-center pb-0 fs-4">BackOffice</h5>
-                    <p class="text-center small">Insert Username and Password</p>
-                  </div>
-                  <div class="fade show" id="alertMessage" role="alert"></div>
-
                   <form method="POST" class="row g-2 needs-validation" id="officeLogin" novalidate>
-                    <span id="error"></span>
-                    <div class="col-12">
-                      <label for="adminUsername" class="form-label">Var1</label>
-                      <div class="input-group has-validation">
-                        <input type="username" name="varone" class="form-control" id="varone" required>
-                        <div class="invalid-feedback">1!</div>
-                      </div>
+                    <div class="fade show" id="alertMessage" role="alert"></div>
+
+                    <div class="col-12 form-floating mt-3">
+                      <textarea rows="6" class="form-control" placeholder="Input Long Text" name="lt" id="longText" style="height: 100px;"></textarea>
+                      <label for="longText">Long Text</label>
                     </div>
                     <div class="col-12">
-                      <button type="submit" class="btn btn-primary w-100" id="submitBtn">Submit</button>
+                      <div class="text-center mt-0">
+                        <h3 class="card-title">Settings</h3>
+                      </div>
+                      <div class="row">
+                        <div class="d-flex justify-content-around">
+                          <div class="">
+                            <input class="form-check-input" type="radio" id="t1" name="t" value="satu" checked>
+                            <label class="form-check-label" for="t1">
+                              Template 1
+                            </label>
+                          </div>
+                          <div class="">
+                            <input class="form-check-input" type="radio" id="t2" name="t" value="dua" checked>
+                            <label class="form-check-label" for="t2">
+                              Template 2
+                            </label>
+                          </div>
+                          <div class="">
+                            <input class="form-check-input" type="radio" id="t3" name="t" value="tiga" checked>
+                            <label class="form-check-label" for="t3">
+                              Template 3
+                            </label>
+                          </div>
+                          <div class="">
+                            <input class="form-check-input" type="radio" id="t4" name="t" value="empat" checked>
+                            <label class="form-check-label" for="t4">
+                              Template 4
+                            </label>
+                          </div>
+                          <div class="">
+                            <input class="form-check-input" type="radio" id="t5" name="t" value="lima" checked>
+                            <label class="form-check-label" for="t5">
+                              Template 5
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mt-3">
+                        <div class="col-md-8">
+                          <li class="list-group-item d-flex justify-content-between">
+                            <div>
+                              <label class="form-check-label">Var 1</label>
+                            </div>
+                            <div class="d-flex align-items-center" style="width:80%;">
+                              <input type="range" name="varone" id="varOne" data-id = "varOneVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                            </div>
+                            <div class="">
+                              <span class="badge bg-primary rounded-pill" id="varOneVal">XX</span>
+                            </div>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between">
+                            <div>
+                              <label class="form-check-label">Var 2</label>
+                            </div>
+                            <div class="d-flex align-items-center" style="width:80%;">
+                              <input type="range" name="vartwo" id="varTwo" data-id = "varTwoVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                            </div>
+                            <div class="">
+                              <span class="badge bg-primary rounded-pill" id="varTwoVal">XX</span>
+                            </div>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between">
+                            <div>
+                              <label class="form-check-label">Var 3</label>
+                            </div>
+                            <div class="d-flex align-items-center" style="width:75%;">
+                              <input type="range" name="varthree" id="varThree" data-id = "varThreeVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                            </div>
+                            <div class="">
+                              <span class="badge bg-primary rounded-pill" id="varThreeVal">XX</span>
+                            </div>
+                          </li>
+                          </ul>
+                        </div>
+                        <div class="col-md-4">
+                          <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-around">
+                              Enable X
+                              <input class="form-check-input me-1" name="enx" type="checkbox" value="X" aria-label="...">
+                            </li>
+                            <li class="list-group-item d-flex justify-content-around">
+                              Enable Y
+                              <input class="form-check-input me-1" name="eny" type="checkbox" value="Y" aria-label="...">
+                            </li>
+                            <li class="list-group-item d-flex justify-content-around">
+                              Yes/No
+                              <input class="form-check-input me-1" name="enyes" type="checkbox" value="Yes" aria-label="...">
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-12 form-floating mt-3">
+                      <textarea rows="6" class="form-control" placeholder="Input Long Text" id="sumText" style="height: 100px;"></textarea>
+                      <label for="sumText">Summarized Text</label>
+                    </div>
+                    <div class="col-12">
+                      <div class="row gap-2">
+                        <div class="col">
+                          <button type="submit" class="btn btn-primary w-100" id="loginBtn">Submit</button>
+                        </div>
+                        <div class="col">
+                          <button type="button" class="btn btn-secondary w-100" id="loginBtn">Copy</button>
+                        </div>
+                      </div>
                     </div>
                   </form>
 
@@ -81,58 +152,19 @@
     </div>
   </main><!-- End #main -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-
-  <script src="assets/vendor/jquery/jquery-3.6.0.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/quill/quill.min.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-  <script>
-    $(document).ready(function() {
-      let loginForm = $('#officeLogin');
-      let msgAlert = $('#alertMessage');
-
-      loginForm.on('submit', function(event) {
-        event.preventDefault();
-        if (!loginForm[0].checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        } else {
-          $.ajax({
-            url: "action.php",
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: 'json',
-            beforeSend: function() {
-              $('#loginBtn').attr('disabled', 'disabled').html = ('Wait...');
-            },
-            success: function(data) {
-              $('#loginBtn').attr('disabled', false);
-              if (data.go != '') {
-                window.location.href  = data.go
-              } else {
-                msgAlert.addClass(data.alert);
-                msgAlert.html(data.message);
-                setTimeout(function() {
-                  msgAlert.removeClass(data.alert).html('');
-                }, 3000);
-                $('#loginBtn').html('Login');
-              }
-            }
-          })
-        }
-
-      });
-
-    });
-  </script>
+  <?php
+  include 'footer.php';
+  ?>
 
 </body>
 
 </html>
+
+<script>
+  $(document).ready(function() {
+    $(document).on('change', '.var', function() {
+      console.log($(this).val());
+      $('#'+$(this).data('id')).html($(this).val());
+    });
+  });
+</script>

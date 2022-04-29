5a29ca367f75ed47576aa7773b5b2972d5fd6108
AzisPrasetyo <azissofyanp@gmail.com>
2022-04-29 13:00:51 +0700

    new ui

--- frontend/index.php
+++ frontend/index.php
@@ -8,178 +8,171 @@ include 'header.php';
 <body>
 
   <main>
-    <div class="container">
-
-      <section class="section d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container g-2">
-          <div class="row justify-content-center">
-            <div class="col-lg-8 d-flex flex-column align-items-center justify-content-center">
-
-              <div class="d-flex justify-content-center py-4">
-                <a href="#" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/logo.png" alt="Logo N">
-                  <span class="d-none d-lg-block fs-3">T5</span>
-                </a>
-              </div><!-- End Logo -->
-
-              <div class="col-lg-12">
-                <form method="POST" class="g-2 needs-validation" id="officeLogin" novalidate>
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col">
-                          <h5 class="card-title">Input News</h5>
-                        </div>
-                        <div class="col d-flex align-items-center justify-content-end gap-2">
-                        <button type="button" id="pasteBtn" class="btn btn-dark btn-sm"><b>Paste</b></i></button>  
-                        <button type="button" id="submitBtn" class="btn btn-primary btn-sm"><b>Send</b><i class="bi bi-plus"></i></button>
-                        </div>
-                      </div>
-                      <div class="fade show" id="alertMessage" role="alert"></div>
-                      <div class="col-12">
-                        <div class="form-floating">
-                          <textarea rows="6" class="form-control" placeholder="Input Long Text" name="lt" id="longText" style="height: 100px;"></textarea>
-                          <label for="longText">Long Text</label>
-                        </div>
-                      </div>
-                      <div class="col-12">
-                        <div class="accordion mt-3" id="accordConf">
-                          <div class="accordion-item">
-                            <h2 class="accordion-header" id="accordConfHead">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseConf" aria-expanded="false" aria-controls="collapseConf">
-                                <b>Settings</b>&nbsp<i>(Default)</i>
-                              </button>
-                            </h2>
-                            <div id="collapseConf" class="accordion-collapse collapse" aria-labelledby="accordConfHead" data-bs-parent="#accordConf">
-                              <div class="accordion-body">
-                                <div class="row">
-                                  <div class="d-flex justify-content-around">
-                                    <div>
-                                      <input class="form-check-input" type="radio" id="t1" name="t" value="satu" checked>
-                                      <label class="form-check-label" for="t1">
-                                        Default
-                                      </label>
-                                    </div>
-                                    <div>
-                                      <input class="form-check-input" type="radio" id="t2" name="t" value="dua" checked>
-                                      <label class="form-check-label" for="t2">
-                                        Template 2
-                                      </label>
-                                    </div>
-                                    <div>
-                                      <input class="form-check-input" type="radio" id="t3" name="t" value="tiga" checked>
-                                      <label class="form-check-label" for="t3">
-                                        Template 3
-                                      </label>
-                                    </div>
-                                    <div>
-                                      <input class="form-check-input" type="radio" id="t4" name="t" value="empat" checked>
-                                      <label class="form-check-label" for="t4">
-                                        Template 4
-                                      </label>
-                                    </div>
-                                    <div>
-                                      <input class="form-check-input" type="radio" id="t5" name="t" value="lima" checked>
-                                      <label class="form-check-label" for="t5">
-                                        Custom
-                                      </label>
-                                    </div>
-                                  </div>
+    <section class="section d-flex flex-column align-items-center justify-content-center py-4">
+      <div class="col-lg-11">
+        
+          <div class="d-flex justify-content-center py-4">
+            <a href="#" class="logo d-flex align-items-center w-auto">
+              <img src="assets/img/logo.png" alt="Logo N">
+              <span class="d-none d-lg-block fs-3">T5</span>
+            </a>
+          </div><!-- End Logo -->
+          <div class="row row-cols-2">
+          <div class="col-7">
+            <form method="POST" class="g-2 needs-validation" id="officeLogin" novalidate>
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title">Input News</h5>
+                    </div>
+                    <div class="col d-flex align-items-center justify-content-end gap-2">
+                      <button type="button" id="pasteBtn" class="btn btn-dark btn-sm"><b>Paste</b></i></button>
+                      <button type="button" id="submitBtn" class="btn btn-primary btn-sm"><b>Send</b><i class="bi bi-plus"></i></button>
+                    </div>
+                  </div>
+                  <div class="fade show" id="alertMessage" role="alert"></div>
+                  <div class="col-12">
+                    <div class="form-floating">
+                      <textarea rows="6" class="form-control" placeholder="Input Long Text" name="lt" id="longText" style="height: 100px;"></textarea>
+                      <label for="longText">Long Text</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="accordion mt-3" id="accordConf">
+                      <div class="accordion-item">
+                        <h2 class="accordion-header" id="accordConfHead">
+                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseConf" aria-expanded="false" aria-controls="collapseConf">
+                            <b>Settings</b>&nbsp<i>(Default)</i>
+                          </button>
+                        </h2>
+                        <div id="collapseConf" class="accordion-collapse collapse" aria-labelledby="accordConfHead" data-bs-parent="#accordConf">
+                          <div class="accordion-body">
+                            <div class="row">
+                              <div class="d-flex justify-content-around">
+                                <div>
+                                  <input class="form-check-input" type="radio" id="t1" name="t" value="satu" checked>
+                                  <label class="form-check-label" for="t1">
+                                    Default
+                                  </label>
                                 </div>
-                                <div class="row mt-3">
-                                  <div class="col-md-8">
-                                    <li class="list-group-item d-flex justify-content-between">
-                                      <div>
-                                        <label class="form-check-label">Var 1</label>
-                                      </div>
-                                      <div class="d-flex align-items-center" style="width:80%;">
-                                        <input type="range" name="varone" id="varOne" data-id="varOneVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
-                                      </div>
-                                      <div class="">
-                                        <span class="badge bg-primary rounded-pill" id="varOneVal">XX</span>
-                                      </div>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between">
-                                      <div>
-                                        <label class="form-check-label">Var 2</label>
-                                      </div>
-                                      <div class="d-flex align-items-center" style="width:80%;">
-                                        <input type="range" name="vartwo" id="varTwo" data-id="varTwoVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
-                                      </div>
-                                      <div class="">
-                                        <span class="badge bg-primary rounded-pill" id="varTwoVal">XX</span>
-                                      </div>
-                                    </li>
-                                    <li class="list-group-item d-flex justify-content-between">
-                                      <div>
-                                        <label class="form-check-label">Var 3</label>
-                                      </div>
-                                      <div class="d-flex align-items-center" style="width:75%;">
-                                        <input type="range" name="varthree" id="varThree" data-id="varThreeVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
-                                      </div>
-                                      <div class="">
-                                        <span class="badge bg-primary rounded-pill" id="varThreeVal">XX</span>
-                                      </div>
-                                    </li>
-                                    </ul>
-                                  </div>
-                                  <div class="col-md-4">
-                                    <ul class="list-group">
-                                      <li class="list-group-item d-flex justify-content-around">
-                                        Enable X
-                                        <input class="form-check-input me-1" name="enx" type="checkbox" value="X" aria-label="...">
-                                      </li>
-                                      <li class="list-group-item d-flex justify-content-around">
-                                        Enable Y
-                                        <input class="form-check-input me-1" name="eny" type="checkbox" value="Y" aria-label="...">
-                                      </li>
-                                      <li class="list-group-item d-flex justify-content-around">
-                                        Yes/No
-                                        <input class="form-check-input me-1" name="enyes" type="checkbox" value="Yes" aria-label="...">
-                                      </li>
-                                    </ul>
-                                  </div>
+                                <div>
+                                  <input class="form-check-input" type="radio" id="t2" name="t" value="dua" checked>
+                                  <label class="form-check-label" for="t2">
+                                    Template 2
+                                  </label>
                                 </div>
+                                <div>
+                                  <input class="form-check-input" type="radio" id="t3" name="t" value="tiga" checked>
+                                  <label class="form-check-label" for="t3">
+                                    Template 3
+                                  </label>
+                                </div>
+                                <div>
+                                  <input class="form-check-input" type="radio" id="t4" name="t" value="empat" checked>
+                                  <label class="form-check-label" for="t4">
+                                    Template 4
+                                  </label>
+                                </div>
+                                <div>
+                                  <input class="form-check-input" type="radio" id="t5" name="t" value="lima" checked>
+                                  <label class="form-check-label" for="t5">
+                                    Custom
+                                  </label>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row mt-3">
+                              <div class="col-md-8">
+                                <li class="list-group-item d-flex justify-content-between">
+                                  <div>
+                                    <label class="form-check-label">Var 1</label>
+                                  </div>
+                                  <div class="d-flex align-items-center" style="width:80%;">
+                                    <input type="range" name="varone" id="varOne" data-id="varOneVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                                  </div>
+                                  <div class="">
+                                    <span class="badge bg-primary rounded-pill" id="varOneVal">XX</span>
+                                  </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between">
+                                  <div>
+                                    <label class="form-check-label">Var 2</label>
+                                  </div>
+                                  <div class="d-flex align-items-center" style="width:80%;">
+                                    <input type="range" name="vartwo" id="varTwo" data-id="varTwoVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                                  </div>
+                                  <div class="">
+                                    <span class="badge bg-primary rounded-pill" id="varTwoVal">XX</span>
+                                  </div>
+                                </li>
+                                <li class="list-group-item d-flex justify-content-between">
+                                  <div>
+                                    <label class="form-check-label">Var 3</label>
+                                  </div>
+                                  <div class="d-flex align-items-center" style="width:75%;">
+                                    <input type="range" name="varthree" id="varThree" data-id="varThreeVal" class="form-range var" min="0" max="5" step="0.5" id="varone">
+                                  </div>
+                                  <div class="">
+                                    <span class="badge bg-primary rounded-pill" id="varThreeVal">XX</span>
+                                  </div>
+                                </li>
+                                </ul>
+                              </div>
+                              <div class="col-md-4">
+                                <ul class="list-group">
+                                  <li class="list-group-item d-flex justify-content-around">
+                                    Enable X
+                                    <input class="form-check-input me-1" name="enx" type="checkbox" value="X" aria-label="...">
+                                  </li>
+                                  <li class="list-group-item d-flex justify-content-around">
+                                    Enable Y
+                                    <input class="form-check-input me-1" name="eny" type="checkbox" value="Y" aria-label="...">
+                                  </li>
+                                  <li class="list-group-item d-flex justify-content-around">
+                                    Yes/No
+                                    <input class="form-check-input me-1" name="enyes" type="checkbox" value="Yes" aria-label="...">
+                                  </li>
+                                </ul>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <div class="col-12 mt-3 ps-3 pe-3 d-flex justify-content-between">
-                      <h6>Session ID : HH883h8hd</h6>
-                      <h6>IP Address : 000.000.000.000</h6>
-                      <h6>Last Request ID : 8J8W8DHW</h6>
-                      </div>
                     </div>
                   </div>
-                </form>
-                <div class="card">
-                  <div class="card-body">
-                  <div class="row">
-                        <div class="col">
-                          <h5 class="card-title">Summarized News</h5>
-                        </div>
-                        <div class="col d-flex align-items-center justify-content-end gap-2">
-                        <button type="button" id="pasteBtn" class="btn btn-dark btn-sm"><b>Copy</b></i></button>
-                        </div>
-                      </div>
-                    <div class="col-12">
-                      <div class="form-floating">
-                        <textarea rows="6" class="form-control" id="sumText" style="height: 100px;"></textarea>
-                        <label for="sumText">Summarized</label>
-                      </div>
-                    </div>
+                  <div class="col-12 mt-3 ps-3 pe-3 d-flex justify-content-between">
+                    <h6>Session ID : HH883h8hd</h6>
+                    <h6>IP Address : 000.000.000.000</h6>
+                    <h6>Last Request ID : 8J8W8DHW</h6>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col-5">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col">
+                    <h5 class="card-title">Summarized News</h5>
+                  </div>
+                  <div class="col d-flex align-items-center justify-content-end gap-2">
+                    <button type="button" id="pasteBtn" class="btn btn-dark btn-sm"><b>Copy</b></i></button>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-floating">
+                    <textarea rows="6" class="form-control" id="sumText" style="height: 100px;"></textarea>
+                    <label for="sumText">Summarized</label>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-    </div>
-
+      </div>
     </section>
-
-    </div>
   </main><!-- End #main -->
 
   <?php

56d72bf58da30431e6d28a8d1f5a770197ce8eeb
AzisPrasetyo <azissofyanp@gmail.com>
2022-04-29 19:32:39 +0700

    split script

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,6 @@
 $(document).ready(function () {
-
-    function updConf (var1,var2,var3,cbx,cby,cbyn) {
-        // Instantiate a slider
-        var mySlider = $("varOne").slider();
-
-            mySlider.slider('setValue', 4);
-        $("#cbX").attr('checked', true);
-        $("#cbY").attr('checked', true);
-        $("#cbYN").attr('checked', cbyn);
-
-    }
-
-  $(document).on("slide", ".var-slide", function () {
+  $(document).on("change", ".var-slide", function () {
     console.log($(this).val());
     $("#" + $(this).data("id")).html($(this).val());
-  });
-
-  $(document).on("change", "[name='tmpl']", function () {
-    console.log($(this).val());
-    updConf ($(this).val());
   });
 });

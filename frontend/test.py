dbce99eb6a76c07dc522096837d3711bb1c5be0e
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2022-05-04 22:22:40 +0700

    test python request

--- frontend/test.py
+++ frontend/test.py
@@ -1,3 +1,14 @@
 import sys
 x = sys.argv[1]
-print(x)
+API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
+headers = {"Authorization": f"Bearer {'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'}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs" : x,
+})
+
+print(output)

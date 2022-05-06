4a0abc863d81077a084c422e1ee90466d570821a
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2022-05-06 20:11:04 +0700

    revert test.py

--- frontend/test.py
+++ frontend/test.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 #import requests
+import requests
 import json
+import re
 import sys
-import base64
 
-x = json.loads(base64.b64decode(sys.argv[1]))
-news = x[':news']
+x = sys.argv[1:]
+API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
+headers = {"Authorization": f"Bearer {'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'}"}
 
-#API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
-#headers = {"Authorization": f"Bearer {'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'}"}
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs" : ' '.join(x)
+})
 
-#def query(payload):
-#	response = requests.post(API_URL, headers=headers, json=payload)
-#	return response.json()
-
-# Join dibawah buat gabungin value dari array tadi yang kepisah pisah jadi 1 kalimat utuh buat request ke api
-#output = query({
-#	"inputs" : ' '.join(news)
-#})
-
-# cuma print summary_text doang
-#print(output[0]['summary_text'])
-print(news)
+print(output[0]['summary_text'])

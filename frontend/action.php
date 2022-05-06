4352f1ce0f437da28b7129058cc7e129cdaa981c
AzisPrasetyo <azissofyanp@gmail.com>
2022-05-06 19:24:10 +0700

    typos parsed data

--- frontend/action.php
+++ frontend/action.php
@@ -123,25 +123,28 @@ if (isset($_POST["action"])) {
 			':cbyn' => $cbyn,
 			':news' => $object->clean_input($_POST["lt"])
 		);
-
-		$pydata = json_encode($data);
-		$enc_pydata = base64_encode($pydata);
+        
+        $pydata = json_encode($data);
+        $enc_pydata = base64_encode($pydata);
+        $command = 'python test.py '.$enc_pydata;
 
 		$gen_req_uniqid = strtoupper(substr(md5(uniqid()), 0, 8));
-
+        
+        
 		$object->query = "
 			INSERT INTO req_tbl 
 			(req_sesid, req_uniqid, req_var1, req_var2, req_var3, req_cb1, req_cb2, req_cb3, req_news) 
 			VALUES 
 			('".$_SESSION['sesid']."', '".$gen_req_uniqid."', :var1, :var2, :var3, :cbx, :cby, :cbyn, :news)
 			";
+		
 
 		$object->execute($data);
-
-		$command = 'python test.py '.$enc_pydata;
-
+        
+        
 		$sum = exec($command);
-
+		
+        
 		$object->query = "
 					UPDATE req_tbl 
 					SET req_sum = '".$sum."' 
@@ -149,7 +152,7 @@ if (isset($_POST["action"])) {
 					";
 
 		$object->execute();
-
+        
 		$alert = 'alert alert-success';
 		$success = 'Summary OK';
 		$output = array(
--- frontend/test.py
+++ frontend/test.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-import requests
+#import requests
 import json
 import sys
 import base64
 
-data = json.loads(base64.b64decode(sys.argv[1]))
+x = json.loads(base64.b64decode(sys.argv[1]))
+news = x[':news']
 
-news = data[':news']
+#API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
+#headers = {"Authorization": f"Bearer {'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'}"}
 
-API_URL = "https://api-inference.huggingface.co/models/csebuetnlp/mT5_multilingual_XLSum"
-headers = {"Authorization": f"Bearer {'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'}"}
-
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+#def query(payload):
+#	response = requests.post(API_URL, headers=headers, json=payload)
+#	return response.json()
 
 # Join dibawah buat gabungin value dari array tadi yang kepisah pisah jadi 1 kalimat utuh buat request ke api
-output = query({
-	"inputs" : ' '.join(news)
-})
+#output = query({
+#	"inputs" : ' '.join(news)
+#})
 
 # cuma print summary_text doang
-print(output[0]['summary_text'])
+#print(output[0]['summary_text'])
+print(news)

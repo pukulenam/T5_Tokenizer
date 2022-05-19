fdcd8ee684a6521dc01d05097ef799b2221c8958
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2022-05-19 13:17:24 +0700

    trying new method to predict

--- frontend/predict.py
+++ frontend/predict.py
@@ -14,28 +14,49 @@ news = x[':news']
 tokenizer = AutoTokenizer.from_pretrained("assamim/t5-small-pukulenam-summarization")
 model = AutoModelForSeq2SeqLM.from_pretrained("assamim/t5-small-pukulenam-summarization", from_tf=True)
 
+# ---------- 001 ----------
 
-WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
+# WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 
-input_ids = tokenizer(
-    [WHITESPACE_HANDLER(news)],
-    return_tensors="pt",
-    padding="max_length",
-    truncation=True,
-    max_length=512
-)["input_ids"]
+# input_ids = tokenizer(
+#     [WHITESPACE_HANDLER(news)],
+#     return_tensors="pt",
+#     padding="max_length",
+#     truncation=True,
+#     max_length=512
+# )["input_ids"]
 
-output_ids = model.generate(
-    input_ids=input_ids,
-    max_length=84,
-    no_repeat_ngram_size=2,
-    num_beams=4
-)[0]
+# output_ids = model.generate(
+#     input_ids=input_ids,
+#     max_length=84,
+#     no_repeat_ngram_size=2,
+#     num_beams=4
+# )[0]
 
-summary = tokenizer.decode(
-    output_ids,
-    skip_special_tokens=True,
-    clean_up_tokenization_spaces=False
-)
+# summary = tokenizer.decode(
+#     output_ids,
+#     skip_special_tokens=True,
+#     clean_up_tokenization_spaces=False
+# )
 
-print(summary)
+# print(summary)
+
+# ---------- 002 ----------
+
+input_ids = tokenizer.encode('summarize: '+news, return_tensors='pt')
+summary_ids = model.generate(input_ids,
+            min_length=20,
+            max_length=80,
+            num_beams=10,
+            repetition_penalty=2.5,
+            length_penalty=1.0,
+            early_stopping=True,
+            no_repeat_ngram_size=2,
+            use_cache=True,
+            do_sample = True,
+            temperature = 0.8,
+            top_k = 50,
+            top_p = 0.95)
+
+summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+print(summary_text)

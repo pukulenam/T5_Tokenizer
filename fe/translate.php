c288ce9077239a8536223ce741ebba6646c85ec4
gabrielkheisa <gabrielkheisa@gmail.com>
2022-06-10 13:32:57 +0700

    test

--- fe/translate.php
+++ fe/translate.php
@@ -1,15 +1,17 @@
 <?php
-include('assets/php/config.php');
+// Pengujian tanpa GCP Google Translate API
+
+//include('assets/php/config.php');
 
 //Query params
 $q = $_REQUEST["q"];
 
-$object->query = "
-    SELECT english FROM translate WHERE user_sesid ='". $_SESSION['sesid'] ."' 
-    ";
+function translate($from_lan, $to_lan, $text){
+    $json = json_decode(file_get_contents('https://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=' . urlencode($text) . '&langpair=' . $from_lan . '|' . $to_lan));
+    $translated_text = $json->responseData->translatedText;
 
-$object->execute();
+    return $translated_text;
+}
 
-$english = $object->fetch();
-echo $english;
+echo translate("id","en", $q);
 ?>

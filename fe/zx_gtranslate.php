0fcd69141b5a6207127a738082d20693173d447d
AzisPrasetyo <azissofyanp@gmail.com>
2022-06-11 11:32:09 +0700

    granslate

--- fe/zx_gtranslate.php
+++ fe/zx_gtranslate.php
@@ -3,14 +3,20 @@ require '../vendor/autoload.php';
 use Google\Cloud\Translate\V2\TranslateClient;
 
 $translate = new TranslateClient([
-    'keyFile' => json_decode(file_get_contents('../g_assets/t5tokenizer-feecafca17ba.json'), true)
+    'keyFile' => json_decode(file_get_contents('../g_assets/t5tokenizer-9c070d9d82ad.json'), true)
 ]);
 
-$source_body = 'Hai Guys, Namaku';
+$source_body = 'Kementerian Komunikasi dan Informatika (Kominfo) memberi lampu hijau pada layanan internet Starlink milik Elon Musk untuk masuk Indonesia. Namun, ada syarat yang wajib dipenuhi layanan tersebut agar bisa beroperasi.
+
+Internet Starlink bisa masuk melalui PT Telkom Satelit Indonesia (Telkomsat). Layanan internet Starlink tersebut nantinya bukan tersedia untuk pelanggan retail, melainkan untuk jaringan tetap tertutup.
+
+Menurut Menteri Komunikasi dan Informatika, Johnny Plate, Kominfo memberikan Hak Labuh Satelit Khusus Non Geostationer (NGSO) kepada PT Telkom Satelit Indonesia (Telkomsat) sebagai pengguna korporat backhaul dalam penyelenggaraan jaringan internet Starlink.';
 $source_lang = 'id';
+$target_lang = 'en';
 
 $result = $translate->translate($source_body, [
-    'target' => 'fr'
+    'source' => $source_lang,
+    'target' => $target_lang
 ]);
 
-echo $result;
+echo $result['text'];

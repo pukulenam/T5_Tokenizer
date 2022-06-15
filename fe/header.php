5ac3dd9dacc590ec36b853f41fe03af9d9ecb24a
gabrielkheisa <gabrielkheisa@gmail.com>
2022-06-15 20:36:58 +0700

    fix typo

--- fe/header.php
+++ fe/header.php
@@ -8,7 +8,6 @@
 
   <!-- Favicons -->
   <link rel="icon" type="image/png" href="/img/puknam.png">
-  <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->

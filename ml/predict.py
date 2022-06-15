9b5e07d9c699f34f69a30eaf9a41956925f2fa1d
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2022-06-15 20:50:28 +0700

    parsing json option final

--- ml/predict.py
+++ ml/predict.py
@@ -11,6 +11,8 @@ import base64
 
 x = json.loads(base64.b64decode(sys.argv[1]))
 news = x[':news']
+early_stopping = True if x[':early_stopping'] == 1 else False
+
 
 # Dissable hf debuging info
 hf_logging.set_verbosity_error()
@@ -20,14 +22,14 @@ model = AutoModelForSeq2SeqLM.from_pretrained("assamim/t5-small-english", from_t
 
 # Regex for news
 WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
-input_ids = tokenizer.encode(WHITESPACE_HANDLER(news2), return_tensors='pt')
+input_ids = tokenizer.encode(WHITESPACE_HANDLER(news), return_tensors='pt')
 summary_ids = model.generate(input_ids,
             min_length=20,
-            max_length=200,
-            num_beams=7,
-            repetition_penalty=2.5,
+            max_length=x[':max_length'],
+            num_beams=x[':num_beam'],
+            repetition_penalty=float(x[':repetition_penalty']),
             length_penalty=1.0,
-            early_stopping=True,
+            early_stopping=early_stopping,
             no_repeat_ngram_size=2,
             use_cache=True,
             do_sample = True,
